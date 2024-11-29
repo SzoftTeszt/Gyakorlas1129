4626a32c10d180045f0ae90cfb48a058cd5db03d
Your Name <you@example.com>
2024-11-29 12:03:14 +0100

    FirebaseAPI

--- src/app/base-fire.service.ts
+++ src/app/base-fire.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/compat/database';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BaseFireService {
 
-  constructor() { }
+  dataRef: AngularFireList<any>
+  constructor(private db: AngularFireDatabase) { 
+    this.dataRef=db.list("/data")
+  }
+  getDatas(){
+    return this.dataRef
+  }
+  addData(data:any){
+    this.dataRef.push(data)
+  }
+  updateData(data:any){
+    let key=data.key
+    delete data.key
+    this.dataRef.update(key, data)
+  }
+
+  deleteData(data:any)  {
+    this.dataRef.remove(data.key)
+  }
+  deleteAllData(){
+    this.dataRef.remove()
+  }
 }
+
--- src/app/data/data.component.ts
+++ src/app/data/data.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FirebaseError } from 'firebase/app';
+import { BaseFireService } from '../base-fire.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-data',
@@ -6,5 +9,38 @@ import { Component } from '@angular/core';
   styleUrl: './data.component.css'
 })
 export class DataComponent {
+  datas:any
+  // { "key": "0", "age": 46, "email": "bob@example.com", "id": 1159, "name": "Eve", "profile_picture": "Unknown" }
+  oszlopok=[
+    {key:"email", text:"E-mail", type:"email"},
+    {key:"name", text:"Name", type:"text"},
+    {key:"age", text:"Age", type:"number"},
+    {key:"profile_picture", text:"Profile Pic", type:"url"},
+  ]
+  newData:any={}
+
+  constructor(private base:BaseFireService){
+    base.getDatas().snapshotChanges().pipe(
+      map(
+        (changes)=>changes.map(
+          (c)=>({key:c.payload.key, ...c.payload.val()})
+        )
+      )
+    ).subscribe(
+      (res)=>this.datas=res
+    )
+
+  }
+
+  addData(){
+    this.base.addData(this.newData)
+    this.newData={}
+  }
+  updateData(data:any){
+    this.base.updateData(data)
+  }
+  deleteData(data:any){
+    this.base.deleteData(data)
+  }
 
 }
